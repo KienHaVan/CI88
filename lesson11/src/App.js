0650af09c76690919bb92a9dd840e0f39011bed5
KienHaVan <hakien351998@gmail.com>
2023-10-09 21:54:51 +0700

    fdf

--- lesson11/src/App.js
+++ lesson11/src/App.js
@@ -5,18 +5,51 @@ const App = () => {
   // const handleGetData = () => {
   //   // then ... catch
   //   axios
-  //     .get('https://6516e61d09e3260018ca7474.mockapi.io/api/v1/products')
+  //     .get('https://6516e61d09e3260018ca7474.mockapi.io/api/v1/productsdfdf')
   //     .then((res) => res.data)
   //     .then((products) => {
   //       console.log(products);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error.message);
   //     });
   // };
   const handleGetData = async () => {
     // async await
-    const { data: products } = await axios.get(
-      'https://6516e61d09e3260018ca7474.mockapi.io/api/v1/products'
+    try {
+      const { data: products } = await axios.get(
+        'https://6516e61d09e3260018ca7474.mockapi.io/api/v1/productssdfd'
+      );
+      console.log(products);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const handleAddData = async () => {
+    await axios.post(
+      'https://6516e61d09e3260018ca7474.mockapi.io/api/v1/products',
+      {
+        name: 'Gorgeous Fresh Pizza hahahah',
+        image:
+          'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/817.jpg',
+        price: '122.00',
+        soldCount: 35892,
+        id: '60',
+      }
     );
-    console.log(products);
+  };
+  const handleEditData = async () => {
+    await axios.put(
+      'https://6516e61d09e3260018ca7474.mockapi.io/api/v1/products/2',
+      {
+        name: 'Kevin',
+      }
+    );
+  };
+  const handleDeleteData = async () => {
+    await axios.delete(
+      'https://6516e61d09e3260018ca7474.mockapi.io/api/v1/products/2'
+    );
   };
   return (
     <div className="max-w-[1280px] mx-auto h-[100vh] h-full w-full flex justify-center items-center gap-4">
@@ -28,13 +61,20 @@ const App = () => {
       </button>
       <button
         className="px-4 py-2 border rounded-lg text-white bg-blue-400"
-        onClick={handleGetData}
+        onClick={handleAddData}
       >
         Add one data
       </button>
       <button
+        className="px-4 py-2 border rounded-lg text-white bg-yellow-400"
+        onClick={handleEditData}
+      >
+        Add one data
+      </button>
+
+      <button
         className="px-4 py-2 border rounded-lg text-white bg-red-400"
-        onClick={handleGetData}
+        onClick={handleDeleteData}
       >
         Delete one data
       </button>
